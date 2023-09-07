e31160c73627763468b3aeb8ad3e592c39da7ee4
MarcosZacarias <marcoszacariasdaniel@gmail.com>
2023-09-07 16:04:55 +0200

    add: component print image

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script>
+import PrintImage from "./components/PrintImage.vue";
+
 export default {
   data() {
     return {
       message: "Hello Word",
     };
   },
+
+  components: { PrintImage },
 };
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+  <PrintImage></PrintImage>
 </template>
 
 <style lang="scss" scoped>
---
+++ src/components/PrintImage.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  data() {
+    return {
+      image: "01.webp",
+    };
+  },
+
+  methods: {
+    imagePath(imageName) {
+      const imageURL = new URL("../assets/img/" + imageName, import.meta.url);
+      return imageURL.href;
+    },
+  },
+};
+</script>
+
+<template>
+  <img :src="imagePath(image)" alt="" />
+</template>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  height: 500px;
+
+  object-fit: cover;
+}
+</style>
